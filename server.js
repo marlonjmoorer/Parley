61e63c3897cfd99d831a6a0fb1e4d7f2aae2c459
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-06 20:01:03 -0400

    more updates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express=require("express")
 var app =express();
 var parser= require("body-parser")
 var ExpressPeerServer = require('peer').ExpressPeerServer;
+var path=  require('path')
 var port=process.env.PORT||5500
 
 app.use(parser.urlencoded({extended:true}))
@@ -20,7 +21,9 @@ app.use("/peer",peerServer)
 //if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 //}
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/build/index.html'));
+  });
 peerServer.on("connection",(id)=>{
     console.log('====================================');
     console.log(id);
