a72b9d1172040262a75b1d1a1a2bffc449cee89f
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-06 21:28:20 -0400

    another one

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports={
    
-    peerOptions:{key: "f8vu238kksnn4s4i",secure:true}
-  // peerOptions:{host:"peepthis.herokuapp.com/",secure:true, port:443, debug: 3 ,path: '/peer'}
+   // peerOptions:{key: "f8vu238kksnn4s4i",secure:true}
+   peerOptions:{host: '/', port: '', path: '/peer'}
 }
