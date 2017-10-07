32937c0532ecf8e2d1667e7545afda7e57e9a876
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-06 20:52:26 -0400

    another one

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports={
    
-   // peerOptions:{host:"localhost", port:process.env.PORT||5500 , path: '/peer'}
-   peerOptions:{host:"peepthis.herokuapp.com/",secure:true, port:443, debug: 3 ,path: '/peer'}
+    peerOptions:{key: process.env.APIKEY}
+  // peerOptions:{host:"peepthis.herokuapp.com/",secure:true, port:443, debug: 3 ,path: '/peer'}
 }
