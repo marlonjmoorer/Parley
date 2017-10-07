a518dee839212b3b8f5f84403f09e80cf4a64480
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-06 21:04:04 -0400

    bad test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports={
    
-    peerOptions:{key: process.env.APIKEY ,secure:true}
+    peerOptions:{key: "f8vu238kksnn4s4i",secure:true}
   // peerOptions:{host:"peepthis.herokuapp.com/",secure:true, port:443, debug: 3 ,path: '/peer'}
 }
