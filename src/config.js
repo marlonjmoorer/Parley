b047bbf2ee8ae4c6660c21d8fdb9ba4a9babe42c
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-06 20:17:32 -0400

    test

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 module.exports={
-    peerOptions:{host:"localhost", port:process.env.PORT||5500 , path: '/peer'}
+   
+   // peerOptions:{host:"localhost", port:process.env.PORT||5500 , path: '/peer'}
+   peerOptions:{host:"https://peepthis.herokuapp.com/", path: '/peer'}
 }
