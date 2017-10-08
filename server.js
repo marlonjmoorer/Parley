a16012bd76f5d78d05cce07d92338c071834c9b0
marlonmoorer <marlonjmoorer@gmail.com>
2017-10-08 12:07:31 -0400

    http fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app =express();
 var parser= require("body-parser")
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 var path=  require('path')
-var port=process.env.PORT||5500
+var port=process.env.PORT||5000
 
 app.use(parser.urlencoded({extended:true}))
 app.use(parser.json())
@@ -18,9 +18,18 @@ var io=require('./io')(server);
 
 app.use("/peer",peerServer)
 
+if(process.env.NODE_ENV=='production'){
+    app.use (function (req, res, next) {
+      var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
+      if (schema === 'https') {
+        next();
+      } else {
+        res.redirect('https://' + req.headers.host + req.url);
+      }
+    });
+};
+
 app.use(express.static('build'));
-
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/build/index.html'));
 });
